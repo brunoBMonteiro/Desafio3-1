d8bd5a9394848d50f6df9977169ac3c3cacbd73a
brunoBMonteiro <brunobilheri@hotmail.com>
2021-02-11 15:28:59 -0300

    iniciando servidor automaticamente com nodemon

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const server = express()
 
 server.get("/", function(req, res){
-    return res.send("Hi")
+    return res.send("Hi, how are you?")
 })
 
 server.listen(5000, function() {
