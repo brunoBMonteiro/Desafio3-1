1723f42041c9fee14dd042650b5a423b1c127135
brunoBMonteiro <brunobilheri@hotmail.com>
2021-02-11 16:21:04 -0300

    trazendo o front end para o servidor

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+
 const server = express()
 
-server.get("/", function(req, res){
-    return res.send("Hi, how are you?")
+server.use(express.static('public'))
+
+server.set("view engine", "html")
+
+nunjucks.configure("views", {
+    express: server
+})
+
+server.get("/", function (req, res){
+    return res.render("sobre")
 })
 
 server.listen(5000, function() {
