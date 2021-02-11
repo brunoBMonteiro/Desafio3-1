bb6612157d4d5727c5b4843d1b1b3bd915b6d457
brunoBMonteiro <brunobilheri@hotmail.com>
2021-02-11 19:24:47 -0300

    configurando arquivos .njk

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = express()
 
 server.use(express.static('public'))
 
-server.set("view engine", "html")
+server.set("view engine", "njk")
 
 nunjucks.configure("views", {
     express: server
@@ -13,6 +13,10 @@ nunjucks.configure("views", {
 
 server.get("/", function (req, res){
     return res.render("sobre")
+})
+
+server.get("/home", function (req, res){
+    return res.render("home")
 })
 
 server.listen(5000, function() {
