aab8c98e433a6e9d3aeb375c981f68637ef44d84
brunoBMonteiro <brunobilheri@hotmail.com>
2021-02-12 15:23:41 -0300

    Rota para erro 404 criada, e pagina de erro criada

--- server.js
+++ server.js
@@ -20,6 +20,11 @@ server.get("/home", function (req, res){
     return res.render("home", { items: cardBlog})
 })
 
+server.use(function(req, res) {
+    res.status(404).render("not-found");
+  });
+
 server.listen(5000, function() {
     console.log("server is running")
 })
+
